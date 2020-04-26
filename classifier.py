0ee339f752c75cb4dd08fb664b7ad1bd09cc4be0
Brent Champion <brent.champion@queensu.ca>
2020-04-26 11:37:35 -0400

    adds keras and splits labels into training and testing

--- classifier.py
+++ classifier.py
@@ -3,6 +3,9 @@ import cv2
 from PIL import Image
 import numpy as np
 
+SAVE_LABELS = True
+LOAD_LABELS = True
+
 data=[]
 labels=[]
 
@@ -26,8 +29,33 @@ for brown in browns:
 # Convert to NP Arrays
 animals = np.array(data)
 labels = np.array(labels)
-np.save("labels/animals",animals)
-np.save("labels/labels",labels)
 
-def main():
-    print("Hello World")
+if SAVE_LABELS:
+    np.save("labels/animals",animals)
+    np.save("labels/labels",labels)
+
+if LOAD_LABELS:
+    animals=np.load("labels/animals.npy")
+    labels=np.load("labels/labels.npy")    
+
+s=np.arange(animals.shape[0])
+np.random.shuffle(s)
+animals=animals[s]
+labels=labels[s]
+
+num_classes=len(np.unique(labels))
+data_length=len(animals)
+
+(x_train,x_test)=animals[(int)(0.1*data_length):],animals[:(int)(0.1*data_length)]
+x_train = x_train.astype('float32')/255
+x_test = x_test.astype('float32')/255
+train_length=len(x_train)
+test_length=len(x_test)
+
+(y_train,y_test)=labels[(int)(0.1*data_length):],labels[:(int)(0.1*data_length)]
+
+import keras
+from keras.utils import np_utils
+#One hot encoding
+y_train=keras.utils.to_categorical(y_train,num_classes)
+y_test=keras.utils.to_categorical(y_test,num_classes)
